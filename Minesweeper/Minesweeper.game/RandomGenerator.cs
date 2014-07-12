1b39a0e7b936eb3a317d29bbd54dd89bd4c9e783
Nachev <n.n.nachev@gmail.com>
2014-07-12 21:03:05 +0300

    Added RandomGenerator class and IRandomGeneratorProvider interface

---
+++ Minesweeper/Minesweeper.game/RandomGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    public class RandomGenerator
+    {
+        private static RandomGenerator instance;
+
+        private RandomGenerator()
+        {
+           
+        }
+        public static RandomGenerator Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
