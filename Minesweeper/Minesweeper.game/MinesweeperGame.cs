2c57a79b2594b35ed51b3e08c7882547c0801cd1
neutrino <jrusev@gmail.com>
2014-07-13 08:31:11 +0300

    minesweeper game made non-static

---
+++ Minesweeper/Minesweeper.game/MinesweeperGame.cs
@@ -0,0 +1,135 @@
+﻿namespace Minesweeper
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class MinesweeperGame
+    {
+        private bool[,] minefield = new bool[5, 10];
+        private bool[,] openedCells = new bool[5, 10];
+        private SortedDictionary<int, string> topScores = new SortedDictionary<int, string>();
+
+        public void Run()
+        {
+            ConsoleManager.Intro();
+            ConsoleManager.DrawGameField();
+
+            AddMines();
+
+            while (true)
+            {
+                string command = ConsoleManager.CommandInput();
+
+                if (command.Equals("restart"))
+                {
+                    break;
+                }
+
+                if (command.Equals("top"))
+                {
+                    ConsoleManager.DisplayHighScores(topScores);
+                    break;
+                }
+
+                if (command.Equals("exit"))
+                {
+                    break;
+                }
+
+                if (command.Length < 3)
+                {
+                    ConsoleManager.ErrorMessage(ErrorType.IllegalInput);
+                    continue;
+                }
+
+                int row = int.Parse(command[0].ToString());
+                int col = int.Parse(command[2].ToString());
+
+                if (openedCells[row, col])
+                {
+                    ConsoleManager.ErrorMessage(ErrorType.IllegalMove);
+                }
+                else
+                {
+                    openedCells[row, col] = true;
+                    if (minefield[row, col])
+                    {
+                        int numberOfOpenedCells = CountOpen() - 1;
+                        ConsoleManager.DrawFinalGameField(minefield, openedCells);
+                        ConsoleManager.Finish(numberOfOpenedCells);
+                        string name = Console.ReadLine();
+                        topScores.Add(numberOfOpenedCells, name);
+                        ConsoleManager.DisplayHighScores(topScores);
+                        break;
+                    }
+
+                    ConsoleManager.OpenCell(row, col, CountNeighborMines(new Position(row, col))); //(row, col));
+                    //DrawGameField();
+                }
+            }
+
+            Console.WriteLine("Good Bye");
+        }
+
+        private void AddMines()
+        {
+            Random random = new Random();
+            for (int i = 0; i < 15; i++)
+            {
+                int index = random.Next(50);
+                while (minefield[(index / 10), (index % 10)])
+                {
+                    index = random.Next(50);
+                }
+
+                minefield[(index / 10), (index % 10)] = true;
+            }
+        }
+
+        private int CountNeighborMines(Position currentPosition) //(int i, int j)
+        {
+            int counter = 0;
+
+            for (int row = -1; row < 2; row++)
+            {
+                for (int col = -1; col < 2; col++)
+                {
+                    if (col == 0 && row == 0)
+                    {
+                        continue;
+                    }
+
+                    if (IsInsideMatrix(currentPosition.Row + row, currentPosition.Col + col) &&
+                        minefield[currentPosition.Row + row, currentPosition.Col + col])
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            return counter;
+        }
+
+        private bool IsInsideMatrix(int row, int col)
+        {
+            return (0 <= row && row <= 4) && (0 <= col && col <= 9);
+        }
+
+        private int CountOpen()
+        {
+            int res = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (openedCells[i, j])
+                    {
+                        res++;
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
