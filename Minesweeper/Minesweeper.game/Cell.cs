1515114ef03824f733a6bad5f498156b9da2f9e2
jasonbashov <triple_x_x_x_@abv.bg>
2014-07-13 16:34:17 +0300

    Revert "testing the commiting "bug""
    This reverts commit 0a25c3eab0fd988912de779c61d7ad36b48b9d42.

--- Minesweeper/Minesweeper.game/Cell.cs
+++ Minesweeper/Minesweeper.game/Cell.cs
@@ -6,7 +6,7 @@
 
     public class Cell : ICell
     {
-        //Commiting testing number 2
+        //Commiting testing
         private int row;
         private int col;
         private bool isOpened;
