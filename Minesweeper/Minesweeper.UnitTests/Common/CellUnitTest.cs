1c1cdec0e540f8869a51bf7c82c7b3aa94879e19
jasonbashov <jbashov@gmail.com>
2014-07-15 17:26:26 +0300

    Added Unit Tests for Cell.cs

---
+++ Minesweeper/Minesweeper.UnitTests/Common/CellUnitTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Minesweeper.Lib;
+
+namespace Minesweeper.UnitTests.Common
+{
+    [TestClass]
+    public class CellUnitTest
+    {
+        [TestMethod]
+        public void TestConstructorCell()
+        {
+            Cell pesho = new Cell();
+            Cell gosho = new Cell();
+            Assert.ReferenceEquals(pesho, gosho);
+        }
+
+        [TestMethod]
+        public void TestIsOpenPropertyInitialization()
+        {
+            Cell pesho = new Cell();
+            Assert.IsFalse(pesho.IsOpened);
+        }
+
+        [TestMethod]
+        public void TestIsMinedPropertyInitialization()
+        {
+            Cell pesho = new Cell();
+            Assert.IsFalse(pesho.IsMined);
+        }
+
+        [TestMethod]
+        public void TestIsFalggedPropertyInitialization()
+        {
+            Cell pesho = new Cell();
+            Assert.IsFalse(pesho.IsFlagged);
+        }
+
+        [TestMethod]
+        public void TestOpenCellMethod()
+        {
+            Cell pesho = new Cell();
+            pesho.OpenCell();
+            Assert.IsTrue(pesho.IsOpened);
+            Assert.IsFalse(pesho.IsFlagged);
+        }
+
+        [TestMethod]
+        public void TestToggleFlagMethod()
+        {
+            Cell pesho = new Cell();
+            Assert.IsFalse(pesho.IsFlagged);
+            pesho.ToggleFlag();
+            Assert.IsTrue(pesho.IsFlagged);
+            pesho.ToggleFlag();
+            Assert.IsFalse(pesho.IsFlagged);
+        }
+
+        [TestMethod]
+        public void TestAddMineMethod()
+        {
+            Cell pesho = new Cell();
+            pesho.AddMine();
+            Assert.IsTrue(pesho.IsMined);
+        }
+    }
+}
